7d2a174449173844e642ef2c436dfe24d718858d
lambn <lambn957@gmail.com>
2024-11-05 09:51:03 +0700

    set up laravel themer

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function() {
-    return 'hehe';
+    return view('welcome');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
